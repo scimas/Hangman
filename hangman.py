292d0dc31ce4811df4ab123f6a588794afcd2ff1
Mihir Gadgil <mmgadgil20@gmail.com>
2018-11-07 12:27:25 -0500

    Generalize to phrases
    Changed variables to refer to phrases instead of movies.

--- hangman.py
+++ hangman.py
@@ -1,10 +1,10 @@
 import random
 import re
 
-with open("movie_list.txt") as infile:
-    movie_list = infile.readlines()
+with open("phrases.txt") as infile:
+    phrase_list = infile.readlines()
 
-used_movies = []
+used_phrases = []
 q = False
 
 print("Welcome to Hangman!")
@@ -17,51 +17,53 @@ print("Here is the first challenge")
 print("Start Guessing")
 print()
 while not q:
-    if len(used_movies) >= len(movie_list):
-        print("You used up all movies O_o")
+    if len(used_phrases) >= len(phrase_list):
+        print("You used up all phrases O_o")
         print("Bye, do some work now!")
         break
 
-    valid_movie = False
-    while not valid_movie:
-        movie = movie_list[random.randrange(len(movie_list))].strip()
-        if movie not in used_movies:
-            used_movies.append(movie)
-            valid_movie = True
+    valid_phrase = False
+    while not valid_phrase:
+        phrase = phrase_list[random.randrange(len(phrase_list))].strip()
+        if phrase not in used_phrases:
+            used_phrases.append(phrase)
+            valid_phrase = True
 
-    compare_movie = movie.lower()
-    guessed_movie = ""
-    for word in movie.split():
-        guessed_movie += "_" * len(word) + " "
-    guessed_movie = guessed_movie.strip()
+    compare_phrase = phrase.lower()
+    guessed_phrase = ""
+    for word in phrase.split():
+        guessed_phrase += "_" * len(word) + " "
+    guessed_phrase = guessed_phrase.strip()
 
     remaining_guesses = 7
     used_characters = []
     print("You have 7 guesses")
 
     while remaining_guesses > 0:
-        print(guessed_movie)
+        print(guessed_phrase)
         print()
 
         valid_input = False
         while not valid_input:
-            guess = input("Make a guess: ")[0]
-            if guess not in used_characters:
+            guess = input("Make a guess: ")
+            if guess not in used_characters and len(guess) == 1:
                 used_characters.append(guess)
+                guess = "[" + guess + "]"
                 valid_input = True
 
-        if re.search(guess, compare_movie):
+        if re.search(guess, compare_phrase):
             new_guess = ""
-            for m in re.finditer(guess, compare_movie):
-                new_guess += guessed_movie[len(new_guess) : m.start()] + movie[m.start()]
-            new_guess += guessed_movie[len(new_guess):]
-            guessed_movie = new_guess
+            for m in re.finditer(guess, compare_phrase):
+                new_guess += guessed_phrase[len(new_guess) : m.start()] + phrase[m.start()]
+            new_guess += guessed_phrase[len(new_guess):]
+            guessed_phrase = new_guess
         else:
             remaining_guesses -= 1
             print("Wrong guess, guesses remianing:", remaining_guesses)
+            print()
 
-        if guessed_movie.lower() == compare_movie:
-            print(movie)
+        if guessed_phrase.lower() == compare_phrase:
+            print(phrase)
             print("Congratulations, you guessed it!")
             print()
             print("Would you like to play again?")
@@ -69,10 +71,14 @@ while not q:
             if play_again != 'y':
                 q = True
                 print("See you later!")
-                break
+            break
 
     if remaining_guesses == 0:
         print("Haha, you lost :D")
+        print("This was the correct answer:")
+        print()
+        print(phrase)
+        print()
         print("Would you like to play again?")
         play_again = input("y/Y for yes, n/N for no: ").lower()
         if play_again != 'y':
